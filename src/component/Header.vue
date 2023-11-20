bc1fd2b8eae51165fedc98a6575cbc9c6377dca3
Lovethrech <94970084+Lovethrech@users.noreply.github.com>
2023-11-20 02:14:39 +0100

    Add files via upload

---
+++ src/component/Header.vue
@@ -0,0 +1,102 @@
+<script setup>
+import { RouterLink } from "vue-router";
+import { ref } from "vue";
+
+const showMenu = ref(false);
+const menuDisplay = ref("");
+const openMenu = () => {
+    menuDisplay.value = "block";
+    return showMenu.value = false;
+}
+const closeMenu = () => {
+    menuDisplay.value = "none";
+    return showMenu.value = true;
+}
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="logo">
+            <img src="/public/logo.svg" alt="">
+        </div>
+        <div class="navbar">
+            <RouterLink active-class="is-active" class="navBtn" to="/">Home</RouterLink>
+            <RouterLink active-class="is-active" class="navBtn" to="">New</RouterLink>
+            <RouterLink active-class="is-active" class="navBtn" to="">Popular</RouterLink>
+            <RouterLink active-class="is-active" class="navBtn" to="">Trending</RouterLink>
+            <RouterLink active-class="is-active" class="navBtn" to="">Categories</RouterLink>
+        </div>
+        <div class="menubar">
+            <i v-if="showMenu" @click="openMenu()" class='bx bx-menu'></i>
+            <i v-else  class='bx bx-x' @click="closeMenu()"></i>
+        </div>
+    </div>
+    <div class="contain-menu">
+        <div class="menu" :style="{display:menuDisplay}">
+            <RouterLink class="navBtn" to="/">Home</RouterLink>
+            <RouterLink class="navBtn" to="/new">New</RouterLink>
+            <RouterLink class="navBtn" to="/popular">Popular</RouterLink>
+            <RouterLink class="navBtn" to="/trend">Trending</RouterLink>
+            <RouterLink class="navBtn" to="/categories">Categories</RouterLink>
+        </div> 
+    </div>
+
+</template>
+<style scoped>
+.container{
+    display:flex;
+    justify-content: space-between;
+}
+.logo img{
+    width: 50px;
+}
+.navbar .navBtn{
+    text-decoration:none;
+    margin:0 10px;
+    color: hsl(0, 0%, 0%);
+    font-weight:500;
+}
+.menubar{
+    font-size:30px;
+    color:hsl(0, 0%, 0%);
+    display:none;
+}
+
+.menu{
+    position:absolute;
+    top:0;
+    right:0;
+    background-color: hsl(0, 100%, 100%);
+    display:none;
+    transition: all .72s;
+}
+.menu .navBtn{
+    text-decoration:none;
+    margin:0 10px;
+    color: hsl(0, 0%, 0%);
+    font-weight:500;
+    display: block;
+}
+
+@media screen and (max-width:650px){
+    .navbar{
+        display:none;
+    }
+    .menubar{
+        display:block;
+        margin:auto 0;
+    }
+    .menu{
+        top:80px;
+        display:block;
+        width:200px;
+        height:60vh;
+        padding:20px;
+    }
+    .menu .navBtn{
+        margin: 20px 0;
+    }
+}
+
+</style>
---
+++ src/view/HomeView.vue
@@ -0,0 +1,226 @@
+<script setup>
+import Header from "../component/Header.vue";
+</script>
+<template>
+    <Header></Header>
+    <div class="headline">
+        <div class="first-section">
+            <div class="image"></div>
+            <div class="content">
+                <div class="head">
+                    <h1 style="font-weight:900;">The Bright Future of Web 3.0?</h1>
+                </div>
+                <div class="text">
+                    <p>The dive into the evolution of the web that claims to put
+                        the power of the platforms back into the hand of the people.
+                        But is it really fulfilling its promise?
+                    </p>
+                    <button>READ MORE</button>
+                </div>
+            </div>
+        </div>
+        <div class="second-section">
+            <h2 style="color:hsl(35, 77%, 62%); font-weight: 600; font-size:30px; margin:10px 0;">New</h2>
+            <div>
+                <h3>Hydrogen VS Electric Cars</h3>
+                <p style="color: hsl(0, 0%, 73%);">Will hydrogen-fueled cars ever catch up to EVs?</p>
+            </div><br><hr><br>
+            <div>
+                <h3>The Downside of AI Artistry</h3>
+                <p style="color: hsl(0, 0%, 73%);">What are the possible adverse effects of on-demand AI image generation?</p>
+            </div><br><hr><br>
+            <div>
+                <h3>Is VC Funding Drying Up?</h3>
+                <p style="color: hsl(0, 0%, 73%);">Private funding by VC firms is down 50% YOY.
+                    We take a look at what that means.
+                </p>
+            </div><br>
+        </div>
+    </div>
+    <div class="lastLine">
+        <div class="contain-content">
+            <div class="image">
+                <img src="/public/image-retro-pcs.jpg" alt="">
+            </div>
+            <div class="text">
+                <h3 style="color:hsl(233, 8%, 79%); font-size:30px;">01</h3>
+                <h4 style="font-size:20px; font-weight:700;">Reviving Retro Pcs</h4>
+                <p style="font-size:16px;">What happens when old PCs are given modern upgrades?</p>
+            </div>
+        </div>
+        <div class="contain-content">
+            <div class="image">
+                <img src="/public/image-top-laptops.jpg" alt="">
+            </div>
+            <div class="text">
+                <h3 style="color:hsl(233, 8%, 79%); font-size:30px;">02</h3>
+                <h4 style="font-size:20px; font-weight:700;">Top 10 Laptops of 2022</h4>
+                <p style="font-size:16px;">Our best pick for various needs and budge.</p>
+            </div>
+        </div>
+        <div class="contain-content">
+            <div class="image">
+                <img src="/public/image-gaming-growth.jpg" alt="">
+            </div>
+            <div class="text">
+                <h3 style="color:hsl(233, 8%, 79%); font-size:30px;">03</h3>
+                <h4 class="title" style="font-size:20px; font-weight:700;">The Growth of Gaming</h4>
+                <p style="font-size:16px;">How the pandemic has sparked fresh opportunities.</p>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.headline{
+    margin: 20px 0 70px;
+    display:flex;
+    gap:20px;
+}
+.first-section{
+    width:50vw;
+    height:90px;
+}
+.first-section .image{
+    background-image:url('/public/image-web-3-desktop.jpg');
+    width: 100%;
+    height:40vh;
+}
+.content{
+    width:100%;
+    display:flex;
+    gap:30px;
+    margin-top:20px;
+}
+.content .head{
+    width:150px;
+    font-weight:900;
+}
+.content .text{
+    font-size: 16px;
+    width: 40vw;
+    text-align: justify;
+}
+.text button{
+    background-color:hsl(5, 85%, 63%);
+    color: hsl(0, 100%, 100%);
+    margin: 20px 0;
+    padding:15px 30px;
+    letter-spacing:5px;
+}
+
+.second-section{
+    background-color: hsl(0, 0%, 0%);
+    color: hsl(0, 100%, 100%);
+    width:30vw;
+    padding: 10px 20px 0;
+}
+
+.lastLine{
+    width:100%;
+    margin:120px 0;
+    display:flex;
+    flex-direction: row;
+    gap:25px;
+}
+.lastLine .contain-content{
+    display:flex;
+    flex-direction: row;
+    gap:15px;
+    height:29vh;
+}
+.lastLine .contain-content .image img{
+    width:8vw;
+    height:100%;
+}
+
+@media screen and (max-width:950px) and (min-width:651px){
+    .lastLine .contain-content{
+        display:block;
+        width:30vw;
+        height:20vh;
+    }
+    .lastLine .contain-content .image img{
+        width:100%;
+        height:30vh;
+    }
+}
+@media screen and (max-width:650px){
+    .headline{
+        display:flex;
+        flex-direction: column;
+    }
+    .first-section{
+        width:100%;
+        height:90px;
+    }
+    .first-section .image{
+        background-image:url('/public/image-web-3-mobile.jpg');
+        width: 100%;
+        height:40vh;
+    }
+    .content{
+        width:100%;
+        display:block;
+    }
+    .content .head{
+        width:100%;
+        font-weight:900;
+    }
+    .content .text{
+        font-size: 16px;
+        width: 100%;
+    }
+    .second-section{
+        background-color: hsl(0, 0%, 0%);
+        color: hsl(0, 100%, 100%);
+        width:100%;
+        margin-top:70vh;
+        padding: 10px 20px 0;
+    }
+
+    .lastLine{
+        display: block;
+    }
+    .lastLine .contain-content{
+        display:flex;
+        width:100%;
+        margin:40px 0;
+    }
+    .lastLine .contain-content .image img{
+        width: 40vw;
+        height:100%;
+    }
+    .contain-content .text{
+        height:40vh;
+    }
+}
+@media screen and (max-width:495px){
+    .lastLine .contain-content .image img{
+        width: 25vw;
+        height:100%;
+    }
+    .contain-content .text p{
+        font-size:14px;
+    }
+}
+@media screen and (max-width:380px) {
+    .second-section{
+        margin-top:80vh;
+    }
+}
+@media screen and (max-width:280px){
+    .second-section{
+        margin-top: 90vh;
+    }
+    .lastLine .contain-content{
+        display:block;
+        width:100%;
+        height:100%;
+        margin: 10px 0;
+    }
+    .lastLine .contain-content .image img{
+        width:100%;
+        height:100%;
+    }
+}
+</style>
